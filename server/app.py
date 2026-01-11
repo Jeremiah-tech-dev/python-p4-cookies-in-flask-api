3c0e44b55e4b116af54a4f45436660631da41890
jeremiah-tech-dev <jeremiah.koske@student.moringaschool.com>
2026-01-11 11:11:52 +0300

    DId the session cookies lab

--- server/app.py
+++ server/app.py
@@ -27,4 +27,23 @@ def show_session(key):
 
 if __name__ == '__main__':
     app.run(port=5555)
-    
+
+@app.route('/sessions/<string:key>', methods=['GET'])
+def show_session(key):
+
+    session["hello"] = session.get("hello") or "World"
+    session["goodnight"] = session.get("goodnight") or "Moon"
+
+    response = make_response(jsonify({
+        'session': {
+            'session_key': key,
+            'session_value': session[key],
+            'session_accessed': session.accessed,
+        },
+        'cookies': [{cookie: request.cookies[cookie]}
+            for cookie in request.cookies],
+    }), 200)
+
+    response.set_cookie('mouse', 'Cookie')
+
+    return response
